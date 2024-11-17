9705a7d2b82284355102f538e219ad4e5c2b3023
eyalp7 <eyalp717@gmail.com>
2024-11-17 09:18:15 +0200

    initial commit

---
+++ client.py
@@ -0,0 +1,95 @@
+import socket
+import ssl
+import os
+import json
+
+#server configuration settings
+IP = "127.0.0.1"
+PORT = 12345
+
+def upload_file(client_socket):
+    """Uploads a file to the server. """
+    files_list = os.listdir()
+    print(f"Available file to upload: {files_list}")
+
+    file_name = input("Enter the file's name: ")
+
+    with open(file_name, "rb") as file: #Opens the file and reads the content.
+        data = file.read()
+    
+    client_socket.send(file_name.encode('utf-8'))
+    client_socket.sendall(data)
+    client_socket.send("<END>".encode('utf-8')) #A flag to tell the server that the upload is completerd.
+
+def receive_file(client_socket):
+    """Receives a file from the server. """
+    files_list = json.loads(client_socket.recv(1024).decode('utf-8')) #The files available to download.
+    print(f"Files avaiable: {files_list}")
+
+    while True:
+        #A loop that runs until the wanted file exists.
+        file_chosen = input("Enter the file that you want to download: ")
+        if file_chosen in files_list:
+            break
+        print("This file does not exist. please choose an existing one. ")
+
+    client_socket.send(file_chosen.encode('utf-8')) #Sends the choice.
+
+    file_packets = b""
+
+    while True:
+        #A loop that runs until the file has been fully sent.
+        data = client_socket.recv(1024)
+        if not data or data == b"<END>":
+            break
+        file_packets += data
+
+    #Creates the file.
+    with open(file_chosen, "wb") as file:
+        file.write(file_packets)
+
+def client(client_socket):
+    while True:
+        #A loop that runs until the client wants to exit the program.
+        os.system('cls') #Clears the cmd.
+        while True:
+            #A loop that runs until the choice is valid.
+            choice = input("""
+Enter your choice:
+upload - upload a file to the server.
+download - get a file from the server.
+exit - exit the program. """)
+            if choice.lower() == "upload" or choice.lower() == "download" or choice.lower() == "exit":
+                break
+            print("Invalid choice, choose on of the available choices. ")
+
+        client_socket.send(choice.encode('utf-8'))
+
+        if choice.lower() == "upload":
+            upload_file(client_socket)
+        elif choice.lower() == "download":
+            receive_file(client_socket)
+        elif choice.lower() == "exit":
+            print("goodbye! ")
+            break
+
+
+def connect_to_server():
+    #Connects to the server with ssl.
+    context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+    context.check_hostname = False  # Disable hostname verification
+    context.verify_mode = ssl.CERT_NONE  # Disable certificate verification
+
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    ssl_socket = context.wrap_socket(client_socket, server_hostname=IP)
+
+    ssl_socket.connect((IP, PORT))
+    try:
+        client(ssl_socket)
+    finally:
+        ssl_socket.close()
+
+
+
+if __name__ == '__main__':
+    connect_to_server()
---
+++ server.py
@@ -0,0 +1,77 @@
+import socket
+import ssl
+import os
+import json
+
+#Server configuration settings.
+IP = "127.0.0.1"
+PORT = 12345
+
+files_folder = r"C:\Users\User\Desktop\files" #The path that holds the server's files.
+
+def receive_file(client_socket):
+    """Receives a file from the client. """
+    file_name = client_socket.recv(1024).decode('utf-8')
+    file_packets = b""
+
+    while True:
+        #A loop that runs until the file is fully sent.
+        data = client_socket.recv(1024)
+        if not data or data == b"<END>":
+            break
+
+        file_packets += data
+    with open(os.path.join(files_folder, file_name), "w") as file: #Creates a file and writes in it.
+        file.write(file_packets.decode('utf-8'))
+
+
+def upload_file(client_socket):
+    """Sends a file to the client. """
+    l_files = os.listdir(files_folder) #All of the files in the directory.
+    client_socket.send(json.dumps(l_files).encode('utf-8'))
+
+    file_name = client_socket.recv(1024).decode()
+    file_path = os.path.join(files_folder, file_name) #The absolute path of the file.
+
+    with open(file_path, "rb") as file:
+        data = file.read()
+    
+    client_socket.sendall(data)
+    client_socket.send("<END>".encode('utf-8'))
+
+
+def handle_client(client_socket):
+    """Handles the main communication with the client. """
+    while True:
+        #A loop that runs until the client leaves.
+        choice = client_socket.recv(1024).decode('utf-8')
+
+        if choice == "upload":
+            receive_file(client_socket)
+        elif choice == "download":
+            upload_file(client_socket)
+        elif choice == "exit":
+            break
+
+def start_server():
+    """Starts a server with ssl implemented."""
+    context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+    context.load_cert_chain(certfile="server.crt", keyfile="server.key") #Using the key and the certificate.
+
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((IP, PORT))
+
+    server_socket.listen(1)
+    print(f"Server is listening on {IP}:{PORT}...")
+
+    while True:
+        connection, address = server_socket.accept()
+        print(f"Connection established from {address}")
+
+        ssl_connection = context.wrap_socket(connection, server_side=True)
+
+        handle_client(ssl_connection)
+        print("client disconnected. ")
+
+if __name__ == "__main__":
+    start_server()
